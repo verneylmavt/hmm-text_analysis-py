0eac2b7ce987e007ab720806bb709d4da325c8f6
verneylmavt <112608297+verneylmavt@users.noreply.github.com>
2023-08-12 18:14:16 +0800

    Eval Script

---
+++ RU/dev_eval_all.ps1
@@ -0,0 +1,5 @@
+python evalResult.py dev.out dev.p1.out
+python evalResult.py dev.out dev.p2.out
+python evalResult.py dev.out dev.p3.2nd.out
+python evalResult.py dev.out dev.p3.8th.out
+python evalResult.py dev.out dev.p4.out
